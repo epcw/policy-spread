5edcd1e3f6c4f6967f5dab1fc7e80443593923c8
Patrick W. Zimmerman <pzimmerman@epcw.org>
2021-11-02 15:20:52 -0700

    kill commented out old shitty version that didn't work.

--- color-cities.js
+++ color-cities.js
@@ -22,27 +22,3 @@ jQuery(window).on('load',function() {
         });
     }, 200);
 });
-
-/*
-jQuery(document).delay(2000).ready(function ($) {
-    $(window).on('load',function() {
-        $("#airports circle").each(function() {
-            if ($(this).attr('r') < 10) {
-                $(this).addClass('red');
-            }
-            if (($(this).attr('r') >= 10 ) && ($(this).attr('r') < 15 )) {
-                $(this).addClass('orange');
-            }
-            if (($(this).attr('r') >= 15 ) && ($(this).attr('r') < 20 )) {
-                $(this).addClass('yellow');
-            }
-            if (($(this).attr('r') >= 20 ) && ($(this).attr('r') < 25 )) {
-                $(this).addClass('lightgreen');
-            }
-            if ($(this).attr('r') > 25) {
-                $(this).addClass('darkgreen');
-            }
-        });
-    });
-});
-*/
