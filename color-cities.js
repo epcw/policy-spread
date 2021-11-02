6057d9d9c01f00b2001442bedbef672b8926a98e
Patrick W. Zimmerman <pkwzimm@gmail.com>
2021-11-02 10:25:07 -0700

    Redid color thresholds to make variations within groups more visible - added purple category.

--- color-cities.js
+++ color-cities.js
@@ -1,10 +1,13 @@
 jQuery(window).on('load',function() {
     setTimeout(function() {
         $("#airports circle").each(function() {
-            if ($(this).attr('r') < 12) {
+            if ($(this).attr('r') < 10) {
+                $(this).addClass('purple');
+            }
+            if (($(this).attr('r') >= 10 ) && ($(this).attr('r') < 16 )) {
                 $(this).addClass('red');
             }
-            if (($(this).attr('r') >= 12 ) && ($(this).attr('r') < 18.5 )) {
+            if (($(this).attr('r') >= 16 ) && ($(this).attr('r') < 18.5 )) {
                 $(this).addClass('orange');
             }
             if (($(this).attr('r') >= 18.5 ) && ($(this).attr('r') < 24 )) {
